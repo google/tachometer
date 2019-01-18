518d4816d37bb48fc2d03e567be72b4ad0e2b409
Alexander Marks <aomarks@google.com>
2019-01-17 17:34:17 -0800

    Get snabbdom recurse benchmark running

--- benchmarks/snabbdom/recurse/index.js
+++ benchmarks/snabbdom/recurse/index.js
@@ -1,4 +1,28 @@
-import {init} from './init.js';
+/**
+ * @license
+ * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt The complete set of authors may be found
+ * at http://polymer.github.io/AUTHORS.txt The complete set of contributors may
+ * be found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by
+ * Google as part of the polymer project is also subject to an additional IP
+ * rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import {registerBenchmark} from '../../../client/lib/index.js';
+
+registerBenchmark('recurse', () => {
+  const data = genXChildData(500);
+  draw(document.body, data, 'hello');
+});
+
+const genXChildData = (depth) => {
+  let xChild = {};
+  while (depth--) {
+    xChild = {xChild};
+  }
+  return xChild;
+};
 
 const h = snabbdom.h;
 const patch = snabbdom.init([]);
@@ -38,5 +62,3 @@ function draw(container, data, string, depth = 0) {
   patch(vnodeOrContainer, newVnode);
   vnode = newVnode;
 }
-
-init('snabbdom', draw);
