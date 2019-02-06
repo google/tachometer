d0da4894a9e7b2b47f6f84169c75077fab5e1e45
Alexander Marks <aomarks@google.com>
2019-02-05 16:42:35 -0800

    Add all of the js-framework-benchmark scripts as variants (keyed).

--- benchmarks/lit-html/js-framework-benchmark-keyed/index.js
+++ benchmarks/lit-html/js-framework-benchmark-keyed/index.js
@@ -155,11 +155,62 @@ _render();
 import {registerBenchmark} from '../../../client/lib/index.js';
 
 const raf = () => new Promise((resolve) => requestAnimationFrame(resolve));
-const runButton = document.getElementById('run');
-const swaprowsButton = document.getElementById('swaprows');
 
-registerBenchmark(async () => {
-  runButton.click();
-  swaprowsButton.click();
+const clickId = (id) => document.getElementById(id).click();
+
+const clickXPath = (xpath) =>
+    document.evaluate(xpath, document.body).iterateNext().click();
+
+registerBenchmark(async (config) => {
+  // The following scripts are based on the scripts from
+  // https://github.com/krausest/js-framework-benchmark/blob/master/webdriver-ts/src/benchmarks.ts
+
+  switch (config.script) {
+    case 'create rows':
+      clickId('add');
+      break;
+
+    case 'replace all rows':
+      clickId('run');
+      break;
+
+    case 'partial update':
+      clickId('run');
+      clickId('update');
+      break;
+
+    case 'select row':
+      clickId('run');
+      clickXPath('//tbody/tr[2]/td[2]/a');
+      break;
+
+    case 'swap rows':
+      clickId('run');
+      clickId('swaprows');
+      break;
+
+    case 'remove row':
+      clickId('run');
+      clickXPath('//tbody/tr[4]/td[3]/a/span[1]');
+      break;
+
+    case 'create many rows':
+      clickId('runlots');
+      break;
+
+    case 'append rows to large table':
+      clickId('run');
+      clickId('add');
+      break;
+
+    case 'clear rows':
+      clickId('run');
+      clickId('clear');
+      break;
+
+    default:
+      throw new Error(`Unknown config.id "${config.id}"`);
+  }
+
   await raf();
 });
