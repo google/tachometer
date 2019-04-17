3e2085d8bac3b74a6f9597b67bd7c9d502b37d98
Alexander Marks <aomarks@google.com>
2019-04-17 11:21:43 -0700

    Neutral check conclusion

--- src/github.ts
+++ src/github.ts
@@ -141,7 +141,7 @@ export async function completeCheckRun(
           // Note that in the future we will likely want to be able to report
           // a failing check (e.g. if there appears to be a slowdown greater
           // than some threshold).
-          conclusion: 'success',
+          conclusion: 'neutral',
           output: {
             title: 'Tachometer Benchmarks',
             summary: 'Benchmark results',
