bd37ad812d1915857554d172f88745a23a08a912
Alexander Marks <aomarks@google.com>
2019-08-20 09:48:00 -0700

    Print a notice if there is a new version of tachometer available.

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,11 @@
 
 require('source-map-support').install();
 
+import {execFile} from 'child_process';
+import {promisify} from 'util';
 import * as path from 'path';
+import ansi = require('ansi-escape-sequences');
+import * as semver from 'semver';
 
 import commandLineUsage = require('command-line-usage');
 
@@ -24,10 +28,51 @@ import {prepareVersionDirectory, makeServerPlans} from './versions';
 import {manualMode} from './manual';
 import {automaticMode} from './automatic';
 
-const getVersion = (): string =>
-    require(path.join(__dirname, '..', 'package.json')).version;
+const installedVersion = (): string =>
+    require(path.join('..', 'package.json')).version;
 
 export async function main(argv: string[]):
+    Promise<Array<ResultStatsWithDifferences>|undefined> {
+  // Don't block anything on a network query to NPM.
+  const latestVersionPromise = latestVersionFromNpm();
+  let results;
+
+  try {
+    results = await realMain(argv);
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  }
+
+  try {
+    notifyIfOutdated(await latestVersionPromise);
+  } catch (e) {
+    // Don't set a non-zero exit code just because the NPM query failed. Maybe
+    // we're behind a firewall and can't contact NPM.
+    console.error(`\nFailed to check NPM for latest version:\n${e}`);
+  }
+
+  return results;
+}
+
+async function latestVersionFromNpm(): Promise<string> {
+  const {stdout} = await promisify(execFile)(
+      'npm', ['info', 'tachometer@latest', 'version']);
+  return stdout.trim();
+}
+
+function notifyIfOutdated(latestVersion: string) {
+  const iv = installedVersion();
+  if (semver.lt(iv, latestVersion)) {
+    console.log(ansi.format(`
+[bold magenta]{Update available!}
+The latest version of tachometer is [green]{${latestVersion}}
+You are running version [yellow]{${iv}}
+See what's new at [cyan]{https://github.com/Polymer/tachometer/blob/master/CHANGELOG.md}`));
+  }
+}
+
+async function realMain(argv: string[]):
     Promise<Array<ResultStatsWithDifferences>|undefined> {
   const opts = parseFlags(argv);
 
@@ -35,7 +80,8 @@ export async function main(argv: string[]):
     console.log(commandLineUsage([
       {
         header: 'tach',
-        content: `v${getVersion()}\nhttps://github.com/PolymerLabs/tachometer`,
+        content:
+            `v${installedVersion()}\nhttps://github.com/PolymerLabs/tachometer`,
       },
       {
         header: 'Usage',
@@ -62,7 +108,7 @@ $ tach http://example.com
   }
 
   if (opts.version) {
-    console.log(getVersion());
+    console.log(installedVersion());
     return;
   }
 
@@ -96,6 +142,7 @@ $ tach http://example.com
 
   if (config.mode === 'manual') {
     await manualMode(config, servers);
+
   } else {
     try {
       return await automaticMode(config, servers);
